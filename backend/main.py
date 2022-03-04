9c4e8af0e14a643a662b1fd1a6a7e0dbb8f6a014
saurabh dhiman <sdhiman@wyest.com>
2022-03-04 11:22:43 +0530

    change from flask to uvicorn
    change from flask to uvicorn

--- backend/main.py
+++ backend/main.py
@@ -1,30 +1,31 @@
-# import uvicorn
-# from utils.logging import logger
+import uvicorn
+from utils.logging import logger
 
-# if __name__ == "__main__":
-# #     uvicorn.run("server.api:app", host="0.0.0.0", port=8081, reload=True, log_config=logger)
-#     uvicorn.run("server.api:app", host="0.0.0.0", port=80, reload=True, log_config=logger)
+if __name__ == "__main__":
+    # uvicorn.run("server.api:app", host="192.168.11.125", port=8081, reload=True, log_config=logger)
+    uvicorn.run("server.api:app", host="localhost", port=8081, reload=True, log_config=logger)
+    # uvicorn.run("server.api:app", host="0.0.0.0", port=80, reload=True, log_config=logger)
 
 # Importing flask module in the project is mandatory
 # An object of Flask class is our WSGI application.
 
-from flask import Flask
-
-# Flask constructor takes the name of
-# current module (__name__) as argument.
-app = Flask(__name__)
-
-# The route() function of the Flask class is a decorator,
-# which tells the application which URL should call
-# the associated function.
-@app.route('/')
-# ‘/’ URL is bound with hello_world() function.
-def hello_world():
-	return 'Hello World'
-
-# main driver function
-if __name__ == '__main__':
-
-	# run() method of Flask class runs the application
-	# on the local development server
-	app.run()
+# from flask import Flask
+#
+# # Flask constructor takes the name of
+# # current module (__name__) as argument.
+# app = Flask(__name__)
+#
+# # The route() function of the Flask class is a decorator,
+# # which tells the application which URL should call
+# # the associated function.
+# @app.route('/')
+# # ‘/’ URL is bound with hello_world() function.
+# def hello_world():
+# 	return 'Hello World'
+#
+# # main driver function
+# if __name__ == '__main__':
+#
+# 	# run() method of Flask class runs the application
+# 	# on the local development server
+# 	app.run()
